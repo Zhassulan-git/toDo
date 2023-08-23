ee09b67cf271e7a34e17969587a086aa2a3dfd79
Zhassulan-git <zhekroi@gmail.com>
2023-08-24 04:29:41 +0600

    add cofig.go file

--- config/config.go
+++ config/config.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+//connect with database
+
 func SetupDb() *sql.DB {
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
 
